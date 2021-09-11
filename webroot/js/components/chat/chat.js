05b6b80b22dcd2082e86abd86cb5fc0b9e4c81cc
Ginger Wong <omqmail@gmail.com>
2021-09-10 23:42:37 -0700

    style the details panel

--- webroot/js/components/chat/chat.js
+++ webroot/js/components/chat/chat.js
@@ -5,12 +5,8 @@ const html = htm.bind(h);
 import Message from './message.js';
 import ChatInput from './chat-input.js';
 import { CALLBACKS, SOCKET_MESSAGE_TYPES } from '../../utils/websocket.js';
-import {
-  jumpToBottom,
-  debounce,
-  setLocalStorage,
-} from '../../utils/helpers.js';
-import { checkIsModerator, extraUserNamesFromMessageHistory } from '../../utils/chat.js';
+import { jumpToBottom, debounce } from '../../utils/helpers.js';
+import { extraUserNamesFromMessageHistory, checkIsModerator } from '../../utils/chat.js';
 import {
   URL_CHAT_HISTORY,
   MESSAGE_JUMPTOBOTTOM_BUFFER,
